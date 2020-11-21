5c04dc9af89531510b03197a51a6c28cf79f4bc7
Mikhail Gudyrin <gudyrmik@gmail.com>
2020-11-21 18:47:51 +0100

    123

---
+++ spec/acceptance/question/add_links_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'User can add links to question', %q{
+  In order to provide additional info to my question
+  As a question's author
+  I'd like to be able to add links
+} do
+
+  given(:user) { create(:user) }
+  given(:link) { 'https://www.google.com/' }
+
+  scenario 'User adds link when asks question' do
+    login(user)
+    visit questions_path
+
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text'
+
+    fill_in 'Link name', with: 'Google'
+    fill_in 'URL', with: link
+
+    click_on 'Ask'
+
+    expect(page).to have_link 'Google', href: link
+  end
+
+end
