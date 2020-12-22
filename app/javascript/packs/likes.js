89f9ec462a2f75e30e6e9f3712f1969c7ddb7e6f
Mikhail Gudyrin <gudyrmik@gmail.com>
2020-12-22 22:06:43 +0100

    refactor

--- app/javascript/packs/likes.js
+++ app/javascript/packs/likes.js
@@ -4,11 +4,6 @@ $(document).on('turbolinks:load', function() {
         let resourceName = e.detail[0]['resourceName'];
         let resourceId = e.detail[0]['resourceId'];
 
-        console.log("rating = " + rating);
-        console.log("resourceName = " + resourceName);
-        console.log("resourceId = " + resourceId);
-        console.log("$('#' + resourceName + '_' + resourceId + ' .like .rating') = " + '#' + resourceName + '_' + resourceId + ' .like .rating');
-
         $('#' + resourceName + '_' + resourceId + ' .like .rating').html(rating)
     })
 });
